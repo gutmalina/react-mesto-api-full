b27e80b779c625a61f998ccb5cbe983203c442cf
Marina Gutkevich <gutmalina@mail.ru>
2022-07-12 13:54:15 +0300

    cors http

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,14 +10,16 @@ const allowedCors = [
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
+  // const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  console.log('origin', origin)
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     // res.header('Access-Control-Allow-Credentials', true);
   }
-  if (method === 'OPTIONS') {
+  console.log('method', req.method)
+  if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.status(200).send();
