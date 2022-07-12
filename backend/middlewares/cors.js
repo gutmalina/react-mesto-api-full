f0f11c19645482cbf8008d66978db7c517fd3f1f
Marina Gutkevich <gutmalina@mail.ru>
2022-07-12 14:29:08 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,6 +7,7 @@ const allowedCors = [
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
+  console.log('headers', req.headers);
   const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
